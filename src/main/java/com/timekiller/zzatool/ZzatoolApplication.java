a1df069052eeec4b9028e094e9fc2b0a19cbcd47
Jaeseong <easeon78@gmail.com>
2024-03-11 16:29:06 +0900

    init: create repository

---
+++ src/main/java/com/timekiller/zzatool/ZzatoolApplication.java
@@ -0,0 +1,13 @@
+package com.timekiller.zzatool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzatoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzatoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timekiller/zzatool/ZzatoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timekiller.zzatool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzatoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
