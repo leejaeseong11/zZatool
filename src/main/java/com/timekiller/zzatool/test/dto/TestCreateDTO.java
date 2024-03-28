e7cd87c9b12425c46f08805c958641a88416cc9b
shpaek <seohpp@gmail.com>
2024-03-28 17:10:52 +0900

    feat: create TestCreateDTO class

---
+++ src/main/java/com/timekiller/zzatool/test/dto/TestCreateDTO.java
@@ -0,0 +1,20 @@
+package com.timekiller.zzatool.test.dto;
+
+import lombok.*;
+
+import java.sql.Date;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TestCreateDTO {
+    private Long testId; // [PK] 테스트 아이디
+    private String testTitle; // 테스트 제목
+    private Long memberId; // [FK] 회원 아이디
+    private Date testDate; // 테스트 제작일
+    private String testImage; // 테스트 이미지
+
+}
