66f4419068e04ed3f05e2cb6254e9a6d7c3fb2f5
Jaeseong <easeon78@gmail.com>
2024-03-28 16:03:15 +0900

    feat: main page roughly

--- src/main/java/com/timekiller/zzatool/test/control/TestController.java
+++ src/main/java/com/timekiller/zzatool/test/control/TestController.java
@@ -1,10 +1,14 @@
 package com.timekiller.zzatool.test.control;
 
+import com.timekiller.zzatool.test.dto.TestDTO;
 import com.timekiller.zzatool.test.service.TestService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,7 +17,9 @@ public class TestController {
     private final TestService testService;
 
     @GetMapping
-    public String main() {
+    public String main(Model model, Pageable pageable) {
+        Page<TestDTO> testList = testService.findTestList(1, pageable);
+        model.addAttribute("tests", testList.getContent());
         return "main";
     }
 }
--- src/main/java/com/timekiller/zzatool/test/service/TestService.java
+++ src/main/java/com/timekiller/zzatool/test/service/TestService.java
@@ -1,3 +1,17 @@
 package com.timekiller.zzatool.test.service;
 
-public interface TestService {}
+import com.timekiller.zzatool.test.dto.TestDTO;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface TestService {
+    /* 테스트 상태에 따른 전체 목록 조회 */
+    Page<TestDTO> findTestList(Integer testStatus, Pageable pageable);
+
+    /* 관리자의 테스트 수정 */
+    void modifyTestByAdmin(Integer testId, TestDTO testDTO);
+
+    /* 관리자의 테스트 삭제 */
+    void removeTestByAdmin(Integer testId);
+}
