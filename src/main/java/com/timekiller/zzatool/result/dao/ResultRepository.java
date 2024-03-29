142653583127fe6fda7d6198f8f8008aa6704b6a
qbobl5 <ddddd991125@gmail.com>
2024-03-30 03:59:56 +0900

    feat: ResultRepository 추가

---
+++ src/main/java/com/timekiller/zzatool/result/dao/ResultRepository.java
@@ -0,0 +1,7 @@
+package com.timekiller.zzatool.result.dao;
+
+import com.timekiller.zzatool.result.entity.Result;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultRepository extends JpaRepository<Result, Long> {}
