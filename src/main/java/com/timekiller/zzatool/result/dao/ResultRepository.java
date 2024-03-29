dc795553319cbbcf9a02a3feef837c496b1ffe49
qbobl5 <ddddd991125@gmail.com>
2024-03-30 04:39:06 +0900

    feat: findOrderByResultDateDesc 추가

--- src/main/java/com/timekiller/zzatool/result/dao/ResultRepository.java
+++ src/main/java/com/timekiller/zzatool/result/dao/ResultRepository.java
@@ -2,6 +2,11 @@ package com.timekiller.zzatool.result.dao;
 
 import com.timekiller.zzatool.result.entity.Result;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResultRepository extends JpaRepository<Result, Long> {}
+public interface ResultRepository extends JpaRepository<Result, Long> {
+    Page<Result> findOrderByResultDateDesc(Example example, Pageable pageable);
+}
