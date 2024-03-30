9dd07671e463332aad0ac68265b61b39befe7358
qbobl5 <ddddd991125@gmail.com>
2024-03-30 21:36:55 +0900

    feat: myResultList-회원이 푼 테스트 목록 조회 호출하는 controller 메소드 추가

--- src/main/java/com/timekiller/zzatool/result/control/ResultController.java
+++ src/main/java/com/timekiller/zzatool/result/control/ResultController.java
@@ -3,20 +3,30 @@ package com.timekiller.zzatool.result.control;
 import com.timekiller.zzatool.result.dto.ResultDTO;
 import com.timekiller.zzatool.result.service.ResultServiceImpl;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@Slf4j
+@RestController
+@RequestMapping("/result")
 public class ResultController {
     @Autowired private ResultServiceImpl resultService;
 
     /* 임시 컨트롤러 */
-    @GetMapping("/result")
+    @GetMapping("/{memberId}/{page}")
     public Page<ResultDTO> myResultList(
-            @RequestParam(value = "memberId") Long memberId, Pageable pageable) {
-        return resultService.findResultListByMemberId(memberId, pageable);
+            @PathVariable("memberId") Long memberId, @PathVariable("page") Integer page)
+            throws Exception {
+        try {
+            Pageable pageable = PageRequest.of(page - 1, 20);
+            return resultService.findResultListByMemberId(memberId, pageable);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
