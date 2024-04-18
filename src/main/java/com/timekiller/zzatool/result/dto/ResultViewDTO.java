a551d728e4b2c7d7dfd850fd45fe0a6cc90c0f69
qbobl5 <ddddd991125@gmail.com>
2024-04-19 02:47:20 +0900

    feat: result view dto 추가

--- src/main/java/com/timekiller/zzatool/result/dto/ResultViewDTO.java
+++ src/main/java/com/timekiller/zzatool/result/dto/ResultViewDTO.java
@@ -1,5 +1,7 @@
 package com.timekiller.zzatool.result.dto;
 
+import com.timekiller.zzatool.test.dto.QuizDTO;
+
 import lombok.*;
 
 @Builder
@@ -8,8 +10,11 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResultViewDTO {
-    private Long quizNo; // 문제 번호
-    private boolean isCorrect; // 정답 여부
-    private Integer myAnswer; // 나의 답
-    private Integer correctAnswer; // 정답
+    private QuizDTO quiz; // 문제
+
+    private Integer myNo; // 나의 답
+    private String myAnswer;
+
+    private Integer correctNo; // 정답
+    private String correctAnswer;
 }
